1da6e8cac31c050e7470e294f33a4b5b462b7d9b
wif_adm_t.sekiguchi <takahiro.sekiguchi@wi2.co.jp>
2017-12-18 11:32:41 +0900

    add develop code .

--- GitTest.java
+++ GitTest.java
@@ -3,7 +3,11 @@ import java.lang.System;
 
 public class GitTest {
     public void main(String args[]){
+        int i = 0;
         System.out.println("Hello,World!");
-        System.out.println("Wi2");
+
+        if(i == 0){
+            i++;
+        }
     }    
 }
