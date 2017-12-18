e17f41dd77932040355f6fafc287d1ae37477e69
wif_adm_t.sekiguchi <takahiro.sekiguchi@wi2.co.jp>
2017-12-18 15:20:22 +0900

    add file code .

--- GitTest3.java
+++ GitTest3.java
@@ -5,5 +5,7 @@ public class GitTest {
     public void main(String args[]){
         int i = 0;
         System.out.println("Hello,World!");
+
+        System.out.println("Hello,World!");
     }    
 }
